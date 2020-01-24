d99255e2a4216661a064478799b877ec0036d95c
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-01-24 08:13:20 -0500

    Add query/setQuery and movies/setMovies useState

--- src/components/searchMovies.js
+++ src/components/searchMovies.js
@@ -2,19 +2,22 @@ import React, {useState} from 'react';
 
 export default function SearchMovies() {
 
+  const [query, setQuery] = useState('');
+
+  const [movies, setMovies] = useState([]);
+
   // state => { input-query, movies }
 
   const searchMovies = async (e) => {
     e.preventDefault();
-
-    const query = 'Transformers';
 
     const Movies_URL = `https://api.themoviedb.org/3/search/movie?api_key=de16157e6a74d761b27aea01fad1ae48&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
       const response = await fetch(Movies_URL);
       const data = await response.json();
-      
+      setMovies(data.results);
+      console.log(data.results);
     } catch (err) {
       console.error(err);
     }
@@ -32,6 +35,8 @@ export default function SearchMovies() {
         className="input"
         type="text"
         name="query"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
         placeholder="i.e. Jurassic Park"
         autoFocus
         spellCheck="false"
