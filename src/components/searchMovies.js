cdea6a51f5662b325c61afc0cc292335eabbce9d
Marcelo Arias <mail.marcelo.as@gmail.com>
2020-01-25 09:02:06 -0500

    Add movie cards with image and basic information

--- src/components/searchMovies.js
+++ src/components/searchMovies.js
@@ -1,7 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 export default function SearchMovies() {
-
   const [query, setQuery] = useState('');
 
   const [movies, setMovies] = useState([]);
@@ -20,28 +19,49 @@ export default function SearchMovies() {
       console.error(err);
     }
 
-
     console.log('submiting');
   };
 
   return (
-    <form className="form" onSubmit={searchMovies}>
-      <label className="label" htmlFor="query">
-        Movie Name
-      </label>
-      <input
-        className="input"
-        type="text"
-        name="query"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="i.e. Jurassic Park"
-        autoFocus
-        spellCheck="false"
-      />
-      <button className="button" type="submit">
-        Search
-      </button>
-    </form>
+    <div>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="i.e. Jurassic Park"
+          autoFocus
+          spellCheck="false"
+        />
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <div className="card" key={movies.id}>
+              <img
+                className="card--image"
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                alt={movie.title + ' poster'}
+                title={movie.title + ' poster'}
+              />
+              <div className="card--content">
+                <h3 className="card--title">{movie.title}</h3>
+                <p><small>Release data: {movie.release_date}</small></p>
+                <p><small>Rating: {movie.vote_average}</small></p>
+                <p className="card--desc">{movie.overview}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
   );
 }
